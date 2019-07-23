c4e1a7f3688f114dc4415a9f7fe8ecaa53442956
Borys Krisko <bvkr@me.com>
2019-07-23 14:27:40 +0300

    Initial Commit

---
+++ WorkWithConstraints/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkWithConstraints
+//
+//  Created by Борис Крисько on 7/23/19.
+//  Copyright © 2019 Borys Krisko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
